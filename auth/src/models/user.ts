eb859d3790d14145880f54c11998ef3f42b81f2d
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-04 19:16:49 -0700

    Add generic bad request error handler and hash password on user create

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface that describes the properties that are required to create a new User
 interface UserAttrs {
@@ -27,6 +28,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+userSchema.pre('save', async function (done) {
+  // only hash the password if the password has been modified
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 // can't add on a build method without add interface UserModel
---
+++ auth/src/services/password.ts
@@ -0,0 +1,24 @@
+// built in crypto node library
+// scrypt is callback base
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+// use promisify to turn scrypt into async await base
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString('hex');
+    // after we hash, we get back a buffer which is like an array with raw data
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return `${buf.toString('hex')}.${salt}}`;
+  }
+
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return hashedPassword === buf.toString('hex');
+  }
+}
