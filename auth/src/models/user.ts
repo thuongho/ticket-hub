06c51520c1c41c0d7f84ecc82f071f6720685fee
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-07 13:16:21 -0700

    Update userSchema to return a structured JSON

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -19,16 +19,28 @@ interface UserDoc extends mongoose.Document {
 }
 
 // these types are only for mongoose, not ts
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      }
+    }
   }
-});
+);
 
 userSchema.pre('save', async function (done) {
   // only hash the password if the password has been modified
