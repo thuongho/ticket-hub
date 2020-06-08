7680c58676ca0d4982899a6851d5a37215677287
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-07 18:39:51 -0700

    Extract current-user cookie check into a middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,15 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-  // Check for cookie with JWT
-  if (!req.session?.jwt) {
-    return res.send({ currentuser: null });
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
   }
-
-  // Decode JWT
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentuser: payload });
-  } catch (err) {
-    res.send({ currentuser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
