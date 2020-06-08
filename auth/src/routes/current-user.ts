692d8d395312805b73b2d3cf882cc57506c38161
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-07 18:18:16 -0700

    Update currentuser and signout

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!!!');
+router.get('/api/users/currentuser', (req: Request, res: Response) => {
+  // Check for cookie with JWT
+  if (!req.session?.jwt) {
+    return res.send({ currentuser: null });
+  }
+
+  // Decode JWT
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentuser: payload });
+  } catch (err) {
+    res.send({ currentuser: null });
+  }
 });
 
 export { router as currentUserRouter };
