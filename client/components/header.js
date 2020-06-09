74d5524504848ff7d66f10b6d69bca70f8fe004d
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-09 12:51:06 -0700

    Build header component

---
+++ client/components/header.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+
+export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Sign in', href: '/auth/signin' },
+    !currentUser && { label: 'Sign up', href: '/auth/signup' },
+    currentUser && { label: 'Sign out', href: '/auth/signout' }
+  ]
+    // filters out only truthy
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className='nav-item'>
+          <Link href={href}>
+            <a className='nav-link'>{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
+  return (
+    <nav className='navbar navbar-light bg-dark' style={{ color: 'white' }}>
+      <Link href='/'>
+        <a className='navbar-brand' style={{ color: 'white' }}>
+          TixWorld
+        </a>
+      </Link>
+
+      <div className='d-flex justify-content-end'>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
+      </div>
+    </nav>
+  );
+};
