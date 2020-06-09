ea7a40d8ddf088c6580290708b34d4f9f9fba98f
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-08 22:04:50 -0700

    Create build client api to make correct api calls base on environment

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import buildClient from '../api/build-client';
 
-export default () => {
+const LandingPage = ({ currentUser }) => {
+  console.log('currentUser', currentUser);
   return <h1>Landing Page1</h1>;
 };
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
