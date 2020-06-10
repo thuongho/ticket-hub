4702a838953cb1572402bcefd1c909cde62c2700
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-09 21:50:56 -0700

    Move errors and middlewares into common and install common in auth

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@thtickets/common';
 
 const router = express.Router();
 
