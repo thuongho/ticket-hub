b280a517dbc679bd93c6056817c292f0399f96ed
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-12 15:13:20 -0700

    Updates

--- common/src/middlewares/require-auth.ts
+++ common/src/middlewares/require-auth.ts
@@ -6,6 +6,7 @@ export const requireAuth = (
   res: Response,
   next: NextFunction
 ) => {
+  console.log('currentUser', req.currentUser);
   if (!req.currentUser) {
     throw new NotAuthorizedError();
   }
