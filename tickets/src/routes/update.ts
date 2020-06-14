ec99add2b14cc66ae0451769c2a325f88d1fc552
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-14 12:37:48 -0700

    Update route saves update and all tests pass
    end of S13

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, request } from 'express';
 import { body } from 'express-validator';
 import {
   requireAuth,
@@ -13,12 +13,27 @@ const router = express.Router();
 router.put(
   '/api/tickets/:id',
   requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
     if (!ticket) {
       throw new NotFoundError();
     }
+
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price
+    });
+    await ticket.save();
 
     res.send(ticket);
   }
