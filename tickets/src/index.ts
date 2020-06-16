2005614e2ba912fd2bffb3271fc0050f163fd9f6
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-16 13:25:08 -0700

    Create NatsWrapper to provide a single nats client for app

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   try {
@@ -10,6 +11,8 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
       throw new Error('MONGO_URI must be defined');
     }
+
+    await natsWrapper.connect('ticketing', 'joker', 'http://nats-srv:4222');
 
     // normally it is mongodb://localhost/table_name
     await mongoose.connect(process.env.MONGO_URI, {
