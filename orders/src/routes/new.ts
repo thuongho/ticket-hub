cf7af0cbe24af41604acf842018c1151954ecbc8
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-17 12:04:17 -0700

    Create Order Model and fix typo on orders mongo deployment

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { requireAuth, validateRequest } from '@thtickets/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      // check valid mongo id
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrdersRouter };
