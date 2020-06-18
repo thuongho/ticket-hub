195045044763520e21d774bd65cb7b56f9e2d5a9
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-18 12:24:47 -0700

    Add tests for indexOrderRouter

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,57 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('fetches orders for a particular user', async () => {
+  const buildTicket = async () => {
+    const ticket = Ticket.build({
+      title: 'Surfs Up',
+      price: 15
+    });
+    await ticket.save();
+    return ticket;
+  };
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  // Create three tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  // Create one order as User #1
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  // Create two orders as User #2
+  const { body: orderOne } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+
+  const { body: orderTwo } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  // Make request to get orders for User #2
+  const response = await request(app)
+    .get('/api/orders')
+    .set('Cookie', userTwo)
+    .expect(200);
+
+  // Make sure we only get the orders for User #2
+  const [orderOneRes, orderTwoRes] = response.body;
+
+  expect(response.body.length).toEqual(2);
+  expect(orderOneRes?.id).toEqual(orderOne.id);
+  expect(orderTwoRes?.id).toEqual(orderTwo.id);
+  expect(orderOneRes?.ticket.id).toEqual(orderOne.ticket.id);
+  expect(orderTwoRes?.ticket.id).toEqual(orderTwo.ticket.id);
+});
--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
 
 it('return an error if the ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -11,3 +13,41 @@ it('return an error if the ticket does not exist', async () => {
     .send({ ticketId })
     .expect(404);
 });
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'Mission Impossible',
+    price: 12
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    userId: 'adfadlfda',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+    ticket
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'Last Samurai',
+    price: 15
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
+
+it.todo('emits an order created event');
