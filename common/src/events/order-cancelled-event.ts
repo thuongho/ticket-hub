2ac4b5ea13450e95f77f252e5bbba9b22943ee33
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-19 13:34:08 -0700

    Swap order cancelled and order created events

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -1,16 +1,11 @@
 import { Subjects } from './types/subjects';
-import { OrderStatus } from './types/order-status';
 
 export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
-    status: OrderStatus;
-    userId: string;
-    expiresAt: Date;
     ticket: {
       id: string;
-      price: number;
     };
   };
 }
--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -1,11 +1,16 @@
 import { Subjects } from './types/subjects';
+import { OrderStatus } from './types/order-status';
 
 export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: Date;
     ticket: {
       id: string;
+      price: number;
     };
   };
 }
