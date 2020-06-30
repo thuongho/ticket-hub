cbda285fb8389ca6f2efa7f8bd77bead76a97b89
Thuong Ho <thuong.t.ho@gmail.com>
2020-06-29 21:43:34 -0700

    Add payment service charge handler

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -4,6 +4,8 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { currentUser, NotFoundError, errorHandler } from '@thtickets/common';
 
+import { createChargeRouter } from './routes/new';
+
 const app = express();
 
 // Middlewares
@@ -18,6 +20,7 @@ app.use(
 app.use(currentUser);
 
 // Route handlers
+app.use(createChargeRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
