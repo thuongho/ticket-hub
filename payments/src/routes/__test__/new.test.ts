93d253f10b361d322f0dc92a861c46e3b15c28cd
Thuong Ho <thuong.t.ho@gmail.com>
2020-07-01 12:06:32 -0700

    Create Payment model and add tests

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import { app } from '../../app';
 import { Order } from '../../models/order';
 import { OrderStatus } from '@thtickets/common';
 import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
 
 // jest.mock('../../stripe.ts');
 
@@ -101,4 +102,11 @@ it('returns a 204 with valid inputs', async () => {
 
   expect(stripeCharge).toBeDefined();
   expect(stripeCharge!.currency).toEqual('usd');
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id
+  });
+
+  expect(payment).not.toBeNull();
 });
