e9b1f1b04393309141c66d9fdb2772b58d5b36de
Thuong Ho <thuong.t.ho@gmail.com>
2020-07-06 18:47:42 -0700

    Route to orderId page on purchase click, modify expires timer to be 1 min

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -13,7 +13,7 @@ import { Ticket } from '../models/ticket';
 import { natsWrapper } from '../nats-wrapper';
 import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
 
-const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+const EXPIRATION_WINDOW_SECONDS = 1 * 60;
 
 const router = express.Router();
 
